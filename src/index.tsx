f6cc7b219158b4ef6455cb0df97465ca5c6042d4
DK <602482790@qq.com>
2024-11-18 15:24:38 +0800

    test commit 1118

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 const queryClient = new QueryClient();
 
 
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
